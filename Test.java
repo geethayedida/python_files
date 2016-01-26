90e4b920bdd6c8b4ac57c27dc46b980b95ca0a32
Geetha Yedida <yedida.geetha@gmail.com>
2016-01-25 21:56:09 -0700

    twelve

---
+++ Test.java
@@ -0,0 +1,22 @@
+package com;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Random;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Random random = new Random();
+		System.out.println(random.nextGaussian());
+		System.out.println(random.nextGaussian());
+		System.out.println(random.nextGaussian());
+		System.out.println(random.nextGaussian());
+		System.out.println(random.nextGaussian());
+		System.out.println(random.nextGaussian());
+		System.out.println(random.nextGaussian());
+		System.out.println(random.nextGaussian());
+		System.out.println(random.nextGaussian());
+	}
+
+}
